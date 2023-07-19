c922d68f8cba4122c6c2fa99c971426d5c9838c8
X-cutor <lekanabass40@gmail.com>
2023-07-20 00:48:58 +0100

    header and nav bar almost fully functiona;

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
+import weatherIcon from '../Images/Weather Icon.png'
+import searchIcon from '../Images/search icon.png'
 
 export default function Header(){
 
     return(
         <header className="main--header">
-            <input className='search--space' type="text" placeholder="search"></input>
+            <div className="icon--container">
+                <img src={weatherIcon} className="weather--icon"></img>
+                <h2>My Weather</h2>
+            </div>
+            <div className="search--space--container">
+                <input className='search--space' type="text" placeholder="search"></input>
+                <img src={searchIcon} className="search--icon"></img>
+            </div>
+            <div className="unit">
+                <select className="temp--unit">
+                    <option>Celsius (&deg;C)</option>
+                    <option>Fahrenheit (&deg;F)</option>
+                </select>
+                
+            </div>
         </header>
     )
 }
+
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,41 @@
 import React from "react";
 
 export default function Navbar(){
-    let options = ['Today','Hourly','Daily','Historical Maps','3 Days','Today','Hourly','Daily','Historical Maps','3 Days','Today','Hourly','Daily','Historical Maps','3 Days']
+    let options = ['Today','Hourly','Daily','Historical Maps','3 Days']
+    // ,'Today1','Hourly1','Daily1','Historical Maps1','3 Days1','Today2','Hourly2','Daily2','Historical Maps2','3 Days2'
     let buttons = options.map(element=>{
-        return(<button className="nav--buttons">{element}</button>)
+        return(<button key={options.indexOf(element)} className="nav--buttons">{element}</button>)
     })
 
+    const scrollableDivRef = React.useRef(null);
+    const [isDragging, setIsDragging] = React.useState(false);
+    const [startPosition, setStartPosition] = React.useState(0);
+
+    function handleMouseDown(event){
+        setIsDragging(true);
+        setStartPosition(event.clientX - scrollableDivRef.current.scrollLeft);
+    }
+
+    function handleMouseUp(){
+        setIsDragging(false);
+    }
+
+    function handleMouseMove(event){
+        if (isDragging) {
+            event.preventDefault();
+            const scrollX = event.clientX - startPosition;
+            scrollableDivRef.current.scrollLeft = scrollX;
+        }
+    }
+
     return(
-        <div className="nav--buttons-container">
+        <div 
+            className="nav--buttons-container"
+            ref={scrollableDivRef}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+        >
             {buttons}
         </div>
     )
