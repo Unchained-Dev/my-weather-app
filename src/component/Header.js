b0f210a3106af421f26c074ad9776d907ed79f22
X-cutor <lekanabass40@gmail.com>
2023-07-20 14:50:34 +0100

    Added suggestions to country search bar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,32 @@
 import React from "react";
 import weatherIcon from '../Images/Weather Icon.png'
 import searchIcon from '../Images/search icon.png'
+import countries from "./countryData";
 
 export default function Header(){
+    let [country,setCountry] = React.useState('')
+    let [suggestions, setSuggestions] = React.useState([])
+
+    function handleChange(event){
+        setCountry(event.target.value)
+    }
+
+    function handleClick(event){
+        setCountry(event.target.textContent)
+    }
+
+    React.useEffect(()=>{
+        let startWith = country
+        setSuggestions(()=>{
+            return(
+                countries.filter(element=>{
+                    return(
+                        country && element.startsWith(startWith) && 
+                        element != startWith)
+                }).map(element=><li onClick={event=>handleClick(event)}>{element}</li>)
+            )
+        })
+    }, [country])
 
     return(
         <header className="main--header">
@@ -15,11 +39,19 @@ export default function Header(){
                 <img src={searchIcon} className="search--icon"></img>
             </div>
             <div className="unit">
+                <div className="country--space--container">
+                    <input 
+                        className="country--space" 
+                        placeholder="Country"
+                        onChange={event=>handleChange(event)}
+                        value={country}>
+                    </input>
+                    <ul className="country--dropdown">{suggestions}</ul>
+                </div>
                 <select className="temp--unit">
-                    <option>Celsius (&deg;C)</option>
-                    <option>Fahrenheit (&deg;F)</option>
-                </select>
-                
+                    <option>&deg;C</option>
+                    <option>&deg;F</option>
+                </select>  
             </div>
         </header>
     )
