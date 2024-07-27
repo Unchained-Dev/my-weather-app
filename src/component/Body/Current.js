9a83615652ac7b9633065e7242d0a6336d9832fd
Unchained Dev <Lekanabass40@gmail.com>
2024-07-26 22:12:38 -0500

    task done

--- src/component/Body/Current.js
+++ src/component/Body/Current.js
@@ -1,48 +1,13 @@
 import React, { useContext } from "react";
 import {SettingsContext} from '../../contexts/settings_context'
 import {WeatherContext} from '../../contexts/weather_context'
+import DailyScroll from "./DailyScroll";
 import descriptions from '../../resources/descriptions.json'
 
 export default function Current(){
     const { theme } = useContext(SettingsContext)
-    const { city, current, paramsMap, daily, utcOffsetSeconds } = useContext(WeatherContext)
+    const { city, current, paramsMap } = useContext(WeatherContext)
     const period = current && current.variables(paramsMap.current.is_day).value() === 1 ? "day" : "night";
-    let dailyMap = ['Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday']
-
-    let dailyRend = dailyMap.map(element=>{
-        return(
-                <div className="daily--forecast" key={element}>
-                    <div className="daily--date">
-                        <h1>{element.getDay? element.getDay() : element}</h1>
-                        <span>26/06</span>
-                    </div>
-                    <span className="daily--maximum">24°C</span>
-                    <span className="daily--minimum">24°C</span>
-                    <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="weather"></img>
-                </div>
-        )
-    })
-
-    const getNextTenDays = (today) => {
-        const daysArray = [];
-    
-        for (let i = 0; i < 10; i++) {
-            const nextDay = new Date(today);
-            nextDay.setDate(today.getUTCDate() + i);
-            daysArray.push(nextDay);
-        }
-    
-        return daysArray;
-    }
-
-    const date = daily ? Number(daily.time()) + utcOffsetSeconds : null
-    const datetime = date? new Date(date * 1000) : undefined
-    
-    const dateArray = datetime? getNextTenDays(datetime) : null;
-    let day = dateArray ? dateArray[0].getUTCDate() : 0;
-    let month = dateArray ? dateArray[0].getUTCMonth() : 0;
-    day = day < 10 ? `0${day}` : day;
-    month = month < 10 ? `0${month}` : month;
 
     return(
         <div className="row">
@@ -67,12 +32,7 @@ export default function Current(){
                         alt="weather"></img>
                 </div>
             </div>
-            <div className={"daily--container " + theme}>
-                <h3>Daily Forecast</h3>
-                <div className="daily">
-                    {dailyRend}
-                </div>
-            </div>
+            <DailyScroll />
         </div>  
     )
 }
---
+++ src/component/Body/DailyScroll.js
@@ -0,0 +1,95 @@
+import React, { useContext } from "react";
+import {SettingsContext} from '../../contexts/settings_context'
+import {WeatherContext} from '../../contexts/weather_context'
+
+export default function DailyScroll() {
+    const { theme } = useContext(SettingsContext)
+    const { daily, utcOffsetSeconds } = useContext(WeatherContext)
+    const scrollableDivRef = React.useRef(null);
+    const [isDragging, setIsDragging] = React.useState(false);
+    const [startPosition, setStartPosition] = React.useState(0);
+
+    function handleMouseDown(event){
+        setIsDragging(true);
+        setStartPosition(event.clientX - scrollableDivRef.current.scrollLeft);
+    }
+
+    function handleMouseUp(){
+        setIsDragging(false);
+    }
+
+    function handleMouseMove(event){
+        if (isDragging) {
+            event.preventDefault();
+            const scrollX = event.clientX - startPosition;
+            scrollableDivRef.current.scrollLeft = scrollX;
+        }
+    }
+    let days = {
+        0: 'Monday',
+        1: 'Tuesday',
+        2: 'Wednesday',
+        3: 'Thursday',
+        4: 'Friday',
+        5: 'Saturday',
+        6: 'Sunday'
+    }
+
+    const date = daily ? Number(daily.time()) + utcOffsetSeconds : null
+    const datetime = date? new Date(date * 1000) : undefined
+
+    let dailyMap;
+    const getNextTenDays = (today) => {
+        const daysArray = [];
+    
+        for (let i = 0; i < 10; i++) {
+            const nextDay = new Date(today);
+            nextDay.setDate(today.getUTCDate() + i);
+            daysArray.push(nextDay);
+        }
+    
+        return daysArray;
+    }
+
+    dailyMap = datetime ? getNextTenDays(datetime) : 
+    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    dailyMap[0].toUTCString && console.log(dailyMap[0].toUTCString())
+
+
+    let dailyRend = dailyMap.map(element=>{
+        let day = element.getUTCDay ? element.getUTCDate() : 0;
+        let month = element.getUTCMonth ? element.getUTCMonth() : 0;
+        day = day < 10 ? `0${day}` : day;
+        month = month < 10 ? `0${month}` : month;
+
+        return(
+                <div 
+                    className="daily--forecast" 
+                    key={element.getDay? element.getDate() : element}
+                >
+                    <div className="daily--date">
+                        <h1>{element.getDay? days[element.getDay()] : element}</h1>
+                        <span>{ day + "/" + month }</span>
+                    </div>
+                    <span className="daily--maximum">24°C</span>
+                    <span className="daily--minimum">24°C</span>
+                    <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="weather"></img>
+                </div>
+        )
+    })
+
+    return(
+        <div className={"daily--container " + theme}>
+            <h3>Daily Forecast</h3>
+            <div 
+                className="daily"
+                ref={scrollableDivRef}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseMove={handleMouseMove}
+            >
+                {dailyRend}
+            </div>
+        </div>
+    )
+}
